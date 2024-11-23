9661b48b1bcaddbe39113743ab7c1e5caa03ed8b
Noah do Rego <ndore051@uottawa.ca>
2024-11-23 13:42:38 -0500

    Set up rough React + Flask interaction

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 
+
 app = Flask(__name__)
 
 
@@ -11,7 +12,7 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
+        frame = camera.get_block()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
 
