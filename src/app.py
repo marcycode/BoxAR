aa75eca21d4ba5631694344a8ff7fa8f998c1ad5
Noah do Rego <ndore051@uottawa.ca>
2024-11-23 22:52:43 -0500

    Added mode selection through query params

--- src/app.py
+++ src/app.py
@@ -15,18 +15,26 @@ def ping():
     return "Successfully pinged"
 
 
-def gen(camera):
+def gen(camera, mode):
     while True:
-        frame = camera.survival_mode()
+        if mode == "survival":
+            frame = camera.survival_mode()
+        elif mode == "scoring-mode":
+            frame = camera.score_mode()
+        elif mode == "free-play":
+            frame = camera.free_mode()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
 
 @app.route("/boxing_feed")
 def boxing_feed():
+    mode = request.args.get("mode")
+    if mode is None:
+        mode = "free-play"
     page_width = int(request.args.get("page_width"))
     page_height = int(request.args.get("page_height"))
     response = Response(
-        gen(VideoCamera(page_width, page_height)),
+        gen(VideoCamera(page_width, page_height), mode),
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
